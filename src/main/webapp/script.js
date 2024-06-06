8e22a2f0476796d0cf870938b16d5ad1e489d335
kiranmai2004 <109661906+kiranmai2004@users.noreply.github.com>
2024-06-06 10:13:21 +0530

    script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -78,7 +78,7 @@ async function removeFromfavorites(id) {
     }
     //Alerting the user and refreshing the page
     alert('Movie Removed from Watchlist');
-    window.location.replace('favorite.html');
+    window.location.replace('fav');
 }
 
 //Displaying the movie list on the search page according to the user list
